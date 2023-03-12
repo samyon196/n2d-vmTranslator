e1dc810d1cb307d833d473789f572cf0a28a5436
samyon196 <samyon.v@gmail.com>
2023-03-12 17:32:32 +0200

    test: started working on pop test (need first work on CPUEmulator

--- src/Instructions/MemoryAccess/PopInstruction.test.ts
+++ src/Instructions/MemoryAccess/PopInstruction.test.ts
@@ -1,19 +1,33 @@
-// import { describe, expect, test } from "vitest";
-// import { PopInstruction } from "./PopInstruction";
-// import { ASMInstruction } from "../Assembly/ASMInstruction";
-// type ASMBlock = Array<ASMInstruction>;
+import { describe, expect, test } from "vitest";
+import { PopInstruction } from "./PopInstruction";
+import { ASMInstruction } from "../Assembly/ASMInstruction";
+import { CInstruction } from "../Assembly/Computation/CInstruction";
+import { AInstruction } from "../Assembly/Addressing/AInstruction";
+type ASMBlock = Array<ASMInstruction>;
 
-// describe("Pop Instruction Tests", () => {
-//   test("Correct object construction", () => {
-//     const popInst = new PopInstruction("local");
-//     expect(popInst).toBeDefined();
-//     expect(popInst.segment).toEqual("local");
-//   });
-//   test("Correct pop local 2 compilation", () => {
-//     const popInst = new PopInstruction("local");
-//     const asmListing: ASMBlock = [
-
-//     ]
-//     expect(popInst.compile()).toEqual("local");
-//   });
-// });
+describe("Pop Instruction Tests", () => {
+  test("Correct object construction", () => {
+    const popInst = new PopInstruction("local");
+    expect(popInst).toBeDefined();
+    expect(popInst.segment).toEqual("local");
+  });
+  test("Correct pop local 2 compilation", () => {
+    const popInst = new PopInstruction("local");
+    const asmListing: ASMBlock = [
+      new AInstruction(0), // Set memory to point at M[0], where SP is located
+      new CInstruction("AM", "M-1", "null"), // Point at stack top and SP--
+      // R13 = M
+      new AInstruction(13)
+      new AInstruction(2),
+      new CInstruction("D", "A", "null"),
+      new AInstruction(1),
+      new CInstruction("A", "M", "null"),
+      new CInstruction("A", "A+D", "null"),
+      // M = tmp
+    ];
+    asmListing.push(new CInstruction("M", "M+1", "null"));
+    asmListing.push(new AInstruction(33));
+    asmListing.push(new AInstruction(33));
+    expect(popInst.compile()).toEqual(asmListing);
+  });
+});
--- src/Instructions/MemoryAccess/PopInstruction.ts
+++ src/Instructions/MemoryAccess/PopInstruction.ts
@@ -1,7 +1,13 @@
 import { VMInstruction } from "../VMInstruction";
+import { ASMInstruction } from '../Assembly/ASMInstruction'
+type ASMBlock = Array<ASMInstruction>;
 export class PopInstruction implements VMInstruction {
   segment: string;
   constructor(segment: string) {
     this.segment = segment;
+  }
+  compile(): ASMBlock {
+    const x: ASMBlock = [];
+    return x;
   }
 }
