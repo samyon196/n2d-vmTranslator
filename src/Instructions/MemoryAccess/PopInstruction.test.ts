3b32e31a8d58591d6b259b06f6c50e834b4017e9
samyon196 <samyon.v@gmail.com>
2023-03-16 12:40:41 +0200

    test: added test for different offset

--- src/Instructions/MemoryAccess/PopInstruction.test.ts
+++ src/Instructions/MemoryAccess/PopInstruction.test.ts
@@ -33,4 +33,26 @@ describe("Pop Instruction Tests", () => {
     ];
     expect(popInst.compile()).toEqual(asmListing);
   });
+  test("should compile correctly the pop local 3", () => {
+    const popInst = new PopInstruction("local", 3);
+    const asmListing: ASMBlock = [
+      // CALCULATE TARGET LCL ADDR
+      new AInstruction("LCL"),
+      new CInstruction("D", "M", "null"),
+      new AInstruction("3"),
+      new CInstruction("D", "A+D", "null"),
+      // STORE TARGET ADDR AT R13
+      new AInstruction("13"),
+      new CInstruction("M", "D", "null"),
+      // GET ELEMENT FROM STACK
+      new AInstruction("SP"),
+      new CInstruction("AM", "M-1", "null"),
+      new CInstruction("D", "M", "null"),
+      // WRITE TO ADDR STORED AT R13
+      new AInstruction("13"),
+      new CInstruction("A", "M", "null"),
+      new CInstruction("M", "D", "null"),
+    ];
+    expect(popInst.compile()).toEqual(asmListing);
+  });
 });
